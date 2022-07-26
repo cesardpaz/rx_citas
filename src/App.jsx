bcf50a2fe95611ebc7b3e9d33784e7802da33245
cesardpaz <cesardpaz@gmail.com>
2022-07-25 22:12:26 -0500

    Listado de pacientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,55 @@
-import { useState } from "react"
+import { useState, useEffect, useRef  } from "react"
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
-  const[pacientes, setPacientes] = useState([]);
+    const [pacientes, setPacientes] = useState([]);
 
-  return (
-    <div className="container mx-auto mt-5 pb-5">
-      <Header />
-      <div className="mt-12 md:flex">
-        <Formulario
-          pacientes    = { pacientes }
-          setPacientes = { setPacientes }
-        />
-        <ListadoPacientes
-          pacientes    = { pacientes }
-        />
-      </div>
-    </div>
-  )
+    const [paciente, setPaciente] = useState({});
+
+    const isMounted = useRef(false)
+
+    useEffect(() => {
+        const obtenerLS = () => {
+        const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+        setPacientes(pacientesLS)
+        }
+        obtenerLS();
+    }, []);
+
+    useEffect(() => {
+        if(isMounted.current){
+            localStorage.setItem('pacientes', JSON.stringify( pacientes ));
+        } else {
+            isMounted.current = true;
+        }
+    }, [pacientes])
+
+    const eliminarPaciente = id => {
+        const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id);
+        setPacientes(pacientesActualizados)
+    }
+
+    return (
+        <div className="container mx-auto mt-5 pb-5">
+            <Header />
+            <div className="mt-12 md:flex">
+                <Formulario
+                    pacientes={pacientes}
+                    setPacientes={setPacientes}
+                    paciente={paciente}
+                    setPaciente={setPaciente}
+                />
+                <ListadoPacientes
+                    pacientes={pacientes}
+                    setPaciente={setPaciente}
+                    eliminarPaciente={eliminarPaciente}
+                />
+            </div>
+        </div>
+    )
 }
 
 export default App
--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,9 @@
-const Error = ({msg}) => {
-  return (
-    <div className="bg-red-800 text-center text-white p-3 uppercase font-bold mb-3 rounded-md">
-        <p>{msg}</p>
-    </div>
-  )
+const Error = ({ msg }) => {
+    return (
+        <div className="bg-red-800 text-center text-white p-3 uppercase font-bold mb-3 rounded-md">
+            <p>{msg}</p>
+        </div>
+    )
 }
 
 export default Error
